deab9c86d6a49e3549cb339d59d35557e07469c9
Ellie S <asila527@gmail.com>
2020-10-22 14:11:47 -0700

    add files

---
+++ rant.js
@@ -0,0 +1,8 @@
+// DEFINE YOUR FUNCTION:
+function rant (str) {
+    console.log(str.toUpperCase());
+    console.log(str.toUpperCase());
+    console.log(str.toUpperCase());
+}
+
+rant('I hate beets!');
